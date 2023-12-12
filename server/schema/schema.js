b0206aa17c2480eef7cb58f9f7bbe12ca4f7d49f
Abdul Samad Gomda <ag7766@nyu.edu>
2023-12-12 20:29:18 +0400

    Add mutations for adding and deleting clients and projects

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,6 +8,7 @@ const {
 	GraphQLString,
 	GraphQLSchema,
 	GraphQLList,
+	GraphQLNonNull,
 } = require("graphql");
 
 // Project Type
@@ -72,6 +73,58 @@ const RootQuery = new GraphQLObjectType({
 	},
 });
 
+// Mutations
+const mutation = new GraphQLObjectType({
+	name: "Mutation",
+	fields: {
+		addClient: {
+			type: ClientType,
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+				email: { type: GraphQLNonNull(GraphQLString) },
+				phone: { type: GraphQLNonNull(GraphQLString) },
+			},
+			resolve(parent, args) {
+				let client = new Client({
+					name: args.name,
+					email: args.email,
+					phone: args.phone,
+				});
+				return client.save();
+			},
+		},
+		// delete client
+		deleteClient: {
+			type: ClientType,
+			args: {
+				id: { type: GraphQLNonNull(GraphQLID) },
+			},
+			resolve(parent, args) {
+				return Client.findByIdAndDelete(args.id);
+			},
+		},
+		addProject: {
+			type: ProjectType,
+			args: {
+				name: { type: GraphQLString },
+				description: { type: GraphQLString },
+				status: { type: GraphQLString },
+				clientId: { type: GraphQLID },
+			},
+			resolve(parent, args) {
+				let project = new Project({
+					name: args.name,
+					description: args.description,
+					status: args.status,
+					clientId: args.clientId,
+				});
+				return project.save();
+			},
+		},
+	},
+});
+
 module.exports = new GraphQLSchema({
 	query: RootQuery,
+	mutation,
 });
